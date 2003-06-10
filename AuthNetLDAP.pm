c2558961d3c2c481488be9b2cff5f62ab3c240c2
Shannon Eric Peevey <speeves@erikin.com>
2003-06-10 15:12:50 +0000

    Import of SPEEVES/Apache-AuthNetLDAP-0.21 from CPAN.
    gitpan-cpan-distribution: Apache-AuthNetLDAP
    gitpan-cpan-version:      0.21
    gitpan-cpan-path:         SPEEVES/Apache-AuthNetLDAP-0.21.tar.gz
    gitpan-cpan-author:       SPEEVES
    gitpan-cpan-maturity:     released

--- AuthNetLDAP.pm
+++ AuthNetLDAP.pm
@@ -3,10 +3,19 @@ package Apache::AuthNetLDAP;
 use strict;
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
 
-require Exporter;
-
 use Net::LDAP;
 use mod_perl;
+
+require Exporter;
+
+@ISA = qw(Exporter AutoLoader);
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+@EXPORT = qw(
+	
+);
+$VERSION = '0.21';
 
 # setting the constants to help identify which version of mod_perl
 # is installed
@@ -27,16 +36,6 @@ BEGIN {
 		Apache::Constants->import('HTTP_UNAUTHORIZED','OK');
 	}
 }
-@ISA = qw(Exporter AutoLoader);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-@EXPORT = qw(
-	
-);
-
-$VERSION = '0.20';
 
 # Preloaded methods go here.
 
